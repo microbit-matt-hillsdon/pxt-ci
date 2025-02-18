65230437b0d7e78939705b3fadeb87fa853f836e
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-02-18 15:15:13 +0000

    More straightforwad build for pxt-microbit
    This one will just use pxt as referenced via npm

---
+++ pxt-microbit-ci.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Hacky CI to build MakeCode in CF Pages and similar environments
+#
+# This script is intended for projects that fork and change pxt-microbit.
+#
+# Intended to be run via
+# 
+# curl -sL https://github.com/microbit-matt-hillsdon/pxt-ci/raw/refs/heads/main/pxt-microbit-ci.sh | bash -s
+#
+# Build output will be in public inside the pxt-microbit checkout.
+# 
+
+set -euxo pipefail
+export CI=true
+
+# This is a thin CLI intended to be installed globally
+npm install -g pxt
+
+npm install
+pxt staticpkg --minify
+cat << EOF > built/packaged/404.html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Not found</title>
+  </head>
+  <body>
+    <p>Not found</p>
+  </body>
+</html>
+EOF
+
+# Use a build path that's the default for CloudFlare 
+mv built/packaged public
