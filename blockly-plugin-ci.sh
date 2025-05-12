d64bd9dca692ee625c27e6e1dcae9e6d84cbb2ec
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-05-12 11:45:23 +0100

    Script to build blockly plugin against v12 branch
    Needed while Blockly's CI is broken

---
+++ blockly-plugin-ci.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# Hacky CI to build the Blockly Keyboard Navigation experiment in CF Pages and
+# similar environments. It builds against rc/v12.0.0 of Blockly via npm link.
+#
+# Intended to be run via
+# 
+# curl -sL
+# https://github.com/microbit-matt-hillsdon/pxt-ci/raw/refs/heads/main/blockly-plugin-ci.sh
+# | bash -s
+#
+# Build output in dist
+
+set -euxo pipefail
+export CI=true
+
+npm install
+
+# Blockly 
+(
+  cd ../
+  git clone git@github.com:google/blockly.git
+  cd blockly
+  # Building our branch for the moment
+  git checkout rc/v12.0.0
+  npm install
+  npm run package
+  cd dist
+  npm link
+)
+
+npm run lint
+npm run format:check
+# leaving test on CF for now
+npm run ghpages
+
+(
+  cat << EOF > built/packaged/404.html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Not found</title>
+  </head>
+  <body>
+    <p>Not found</p>
+  </body>
+</html>
+EOF
+)
+
+# Use a build path that's the default for CloudFlare 
+mv ../pxt-microbit/built/packaged public
